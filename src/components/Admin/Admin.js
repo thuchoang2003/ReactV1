1f3be4661fcb038c62c0633d7dc0960003c106f2
unknown <nguyenthuchoang17112003@gmail.com>
2023-06-14 22:05:00 +0700

    design sidebar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,4 +1,22 @@
+import Sidebar from "../Sidebar/Sidebar";
+import "../../assets/scss/Admin.scss";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 const Admin = (props) => {
-  return <div>Admin components</div>;
+  const [collapsed, setcollapsed] = useState(false);
+  return (
+    <div className="admin-container">
+      <div className="admin-sidebar">
+        <Sidebar collapsed={collapsed} />
+      </div>
+      <div className="admin-content">
+        <FaBars
+          onClick={() => {
+            setcollapsed(!collapsed);
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 export default Admin;
